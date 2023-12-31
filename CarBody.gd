52d56ca59bebab62448ab249d9ada64dfc8ef772
Drac <gpt.sahaj28@gmail.com>
2023-12-31 14:09:04 +0530

    First commit

---
+++ CarBody.gd
@@ -0,0 +1,17 @@
+extends VehicleBody3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+const MAX_STER = 0.8
+const ENGINE_POWER = 300
+func _process(delta):
+	steering = move_toward(steering, 
+	Input.get_axis("ui_right","ui_left") * MAX_STER,
+	delta*2.5)
+	engine_force = Input.get_axis("ui_up", "ui_down") * ENGINE_POWER
+	print_debug(Input.get_axis("ui_up","ui_down") * ENGINE_POWER)
